109a5bbc2d6701d2dd00ebbff8aadd94087d702f
heitorfreitasferreira <heitor.ff@hotmail.com>
2024-06-25 00:49:19 -0300

    refactor: Rename database repository interface and implementation for tasks
    This commit renames the database repository interface and implementation for tasks. The interface `TarefaRepository` is renamed to `TaskRepository` to improve clarity and consistency. The implementation methods are also updated to match the new naming convention.

--- internal/database/tarefa_repo.go
+++ internal/database/tarefa_repo.go
@@ -3,32 +3,32 @@ package database
 import "raw-sqlite/internal/models"
 
 type TaskRepository interface {
-	CreateTask(tarefa *models.Tarefa) error
-	GetTaskByID(id int) (*models.Tarefa, error)
-	UpdateTask(tarefa *models.Tarefa) error
+	CreateTask(tarefa *models.Task) error
+	GetTaskByID(id int) (*models.Task, error)
+	UpdateTask(tarefa *models.Task) error
 	DeleteTask(id int) error
-	GetAllTasksByProjectID(projetoID int) ([]*models.Tarefa, error)
-	GetAllTasks() ([]*models.Tarefa, error)
+	GetAllTasksByProjectID(projetoID int) ([]*models.Task, error)
+	GetAllTasks() ([]*models.Task, error)
 }
 
-func (s *service) CreateTask(tarefa *models.Tarefa) error {
+func (s *service) CreateTask(tarefa *models.Task) error {
 	_, err := s.db.Exec("INSERT INTO tasks (name, description, start_date, end_date, status, project_id) VALUES (?, ?, ?, ?, ?, ?)",
-		tarefa.Nome, tarefa.Descricao, tarefa.DataInicio, tarefa.DataTermino, tarefa.Status, tarefa.ProjetoID)
+		tarefa.Name, tarefa.Description, tarefa.StartDate, tarefa.EndDate, tarefa.Status, tarefa.ProjectId)
 	return err
 }
 
-func (s *service) GetTaskByID(id int) (*models.Tarefa, error) {
-	tarefa := &models.Tarefa{}
-	err := s.db.QueryRow("SELECT * FROM tasks WHERE id = ?", id).Scan(&tarefa.ID, &tarefa.Nome, &tarefa.Descricao, &tarefa.DataInicio, &tarefa.DataTermino, &tarefa.Status, &tarefa.ProjetoID)
+func (s *service) GetTaskByID(id int) (*models.Task, error) {
+	tarefa := &models.Task{}
+	err := s.db.QueryRow("SELECT * FROM tasks WHERE id = ?", id).Scan(&tarefa.ID, &tarefa.Name, &tarefa.Description, &tarefa.StartDate, &tarefa.EndDate, &tarefa.Status, &tarefa.ProjectId)
 	if err != nil {
 		return nil, err
 	}
 	return tarefa, nil
 }
 
-func (s *service) UpdateTask(tarefa *models.Tarefa) error {
+func (s *service) UpdateTask(tarefa *models.Task) error {
 	_, err := s.db.Exec("UPDATE tasks SET name = ?, description = ?, start_date = ?, end_date = ?, status = ?, project_id = ? WHERE id = ?",
-		tarefa.Nome, tarefa.Descricao, tarefa.DataInicio, tarefa.DataTermino, tarefa.Status, tarefa.ProjetoID, tarefa.ID)
+		tarefa.Name, tarefa.Description, tarefa.StartDate, tarefa.EndDate, tarefa.Status, tarefa.ProjectId, tarefa.ID)
 	return err
 }
 
@@ -37,17 +37,17 @@ func (s *service) DeleteTask(id int) error {
 	return err
 }
 
-func (s *service) GetAllTasksByProjectID(projetoID int) ([]*models.Tarefa, error) {
+func (s *service) GetAllTasksByProjectID(projetoID int) ([]*models.Task, error) {
 	rows, err := s.db.Query("SELECT * FROM tasks WHERE project_id = ?", projetoID)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var tasks []*models.Tarefa
+	var tasks []*models.Task
 	for rows.Next() {
-		tarefa := &models.Tarefa{}
-		err = rows.Scan(&tarefa.ID, &tarefa.Nome, &tarefa.Descricao, &tarefa.DataInicio, &tarefa.DataTermino, &tarefa.Status, &tarefa.ProjetoID)
+		tarefa := &models.Task{}
+		err = rows.Scan(&tarefa.ID, &tarefa.Name, &tarefa.Description, &tarefa.StartDate, &tarefa.EndDate, &tarefa.Status, &tarefa.ProjectId)
 		if err != nil {
 			return nil, err
 		}
@@ -61,17 +61,17 @@ func (s *service) GetAllTasksByProjectID(projetoID int) ([]*models.Tarefa, error
 	return tasks, nil
 }
 
-func (s *service) GetAllTasks() ([]*models.Tarefa, error) {
+func (s *service) GetAllTasks() ([]*models.Task, error) {
 	rows, err := s.db.Query("SELECT * FROM tasks")
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var tasks []*models.Tarefa
+	var tasks []*models.Task
 	for rows.Next() {
-		tarefa := &models.Tarefa{}
-		err = rows.Scan(&tarefa.ID, &tarefa.Nome, &tarefa.Descricao, &tarefa.Responsavel, &tarefa.DataInicio, &tarefa.DataTermino, &tarefa.Status, &tarefa.ProjetoID)
+		tarefa := &models.Task{}
+		err = rows.Scan(&tarefa.ID, &tarefa.Name, &tarefa.Description, &tarefa.Owner, &tarefa.StartDate, &tarefa.EndDate, &tarefa.Status, &tarefa.ProjectId)
 		if err != nil {
 			return nil, err
 		}
